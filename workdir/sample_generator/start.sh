6e44ef811bf49569dbb9dc60edf76b1fee256da0
Abu <root@chakra>
2024-07-18 01:10:48 +0300

    Better redis connection from shell generator

--- workdir/sample_generator/start.sh
+++ workdir/sample_generator/start.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
-generator_id=$(head -1 ./redis.conf)
-password=$(tail -1 ./redis.conf)
-nc 172.16.0.1 6379 <<EOF
-auth $generator_id $password
-set '$generator_id:loaded at' '$(date)'
-EOF
+
+cleanup() {
+    echo shutdown > redis-in
+    kill $nc_pid
+    kill $cat_pid
+    exec 3>&-
+    wait
+    rm -f redis-in redis-pipe redis-out
+}
+trap cleanup EXIT ERR
+
+generator_id=$(head -1 ./redis.auth)
+password=$(tail -1 ./redis.auth)
+mkfifo redis-in redis-pipe
+nc 172.16.0.1 6379 < redis-in > redis-pipe &
+nc_pid=$!
+cat redis-pipe > redis-out &
+cat_pid=$!
+exec 3>redis-in
+redis() { echo "$*" > redis-in && tail -1 redis-out; }
+redis "auth $generator_id $password"
+
+tag() { redis "set '$generator_id:$1' '$2'"; }
+tag "loaded at" "$(date)"
